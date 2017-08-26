7a5e081d060ae385b0be4ae4226d45eefc85be85
khardi <kharchilindanila@gmail.com>
2017-08-26 21:57:31 +0300

    try doctest

---
+++ source/test_vector/vector.py
@@ -0,0 +1,19 @@
+from math import hypot
+
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __abs__(self):
+        return hypot(self.__x, self.__y)
